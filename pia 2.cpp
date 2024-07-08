2fab0a7ab165fe08c4e759bb59802877a7bd4171
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-07-08 10:09:00 -0300

    Update pia 2.cpp

--- pia 2.cpp
+++ pia 2.cpp
@@ -20,9 +20,9 @@ static void ejercicio1(){
         }
     }
     for(i=0; i<n; i++){
+        // incremento = incremento + 2;
         for(j=0; j<m; j++){
-            incremento = incremento + 2;
-            matriz[incremento][j] = sqrt2;
+            matriz[2][j] = sqrt2;
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
@@ -32,18 +32,37 @@ static void ejercicio1(){
 }
 static void ejercicio2(){
     int matriz[4][3];
-    int i=0, j=0, n=4, m=3, num=0;
+    int i=0, j=0, n=4, m=3, num=0, incremento=0, num2=0;
     
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
             printf("Ingrese un numero : ");
             scanf("%d" ,&num);
-            matriz[i][j] = num;
+            num2 = num;
+            matriz[i][j] = num2;
         }
     }
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
-            printf(" %d ",matriz[i][j]);
+            printf("%d ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----------------------\n");
+    //primera fila
+    printf("primera fila\n");
+    for(i=0; i<1; i++){
+        for(j=0; j<n; j++){
+            printf("%d ",matriz[0][j]);
+        }
+        printf("\n");
+    }
+    //ultima fila
+    printf("----------------------\n");
+    printf("segunda fila\n");
+    for(i=0; i<1; i++){
+        for(j=0; j<n; j++){
+            printf("%d ",matriz[2][j]);
         }
         printf("\n");
     }
